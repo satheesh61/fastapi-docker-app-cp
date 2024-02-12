b0e2e1abf7f14b6f87d568b8103cb0751bf424c5
satheesh61 <satheeshkumar.u@thedatateam.com>
2024-02-13 02:51:10 +0530

    updated the buildspec and appspec

--- deployment_scripts/stop_remove_docker_image.sh
+++ deployment_scripts/stop_remove_docker_image.sh
@@ -3,7 +3,7 @@
 
 # # Authenticate Docker with ECR
 # $(aws ecr get-login --no-include-email --region ap-south-1)
-
+API_ECR_REPOSITORY_URI=<repo url>
 # stop the docker compose
 cd /home/ec2-user/app
 ls
